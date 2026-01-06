7588a2019136ca8e5c533a254ab36633fab4f5df
nguyentienphat2904 <phat.nguyen221252729@hcmut.edu.vn>
2026-01-06 16:51:56 +0700

    platform is mobile

--- app/api/auth/authorize+api.ts
+++ app/api/auth/authorize+api.ts
@@ -26,7 +26,7 @@ export async function GET(request: Request) {
   if (redirectUri === APP_SCHEME) {
     platform = "mobile";
   } else if (redirectUri === BASE_URL) {
-    platform = "web";
+    platform = "mobile";
   } else {
     return Response.json({ error: "Invalid redirect_uri" }, { status: 400 });
   }
